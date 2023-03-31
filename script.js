8c5d157f86c5f8a83ea953eb5ca6dc86262b8b87
Pietro <pietro.morelli@gmail.com>
2023-03-31 11:56:00 -0300

    Add first tentative method for calculations.

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ let displayValue = "";
 const numberButtons = document.querySelectorAll('[data-function="number"]');
 const calcButtons = document.querySelectorAll('[data-function="calc"]');
 const display = document.querySelector('.display');
+const clearButton = document.querySelector('#clear');
+const equalsButton = document.querySelector('#equals')
 
 
 
@@ -14,12 +16,17 @@ const display = document.querySelector('.display');
 numberButtons.forEach(button => 
     button.addEventListener('click', e => {
         const num = e.target.textContent;
+        if (display.textContent === "0"){
+            displayValue = num;
+        }
+        else {
+            displayValue += num;
+        };
         currentNumber += num;
-        displayValue += num;
         console.log(num);
         updateDisplay();
 
-    }));
+}));
 
 calcButtons.forEach(button => 
     button.addEventListener('click', e => {
@@ -31,11 +38,50 @@ calcButtons.forEach(button =>
         displayValue = "0";
         updateDisplay();
 
-    }));
+}));
 
+equalsButton.addEventListener('click', () => {
+    calcResult(oldNumber, operator, currentNumber);
+})
+
+
+
+clearButton.addEventListener('click', () => {
+    clearAll();
+});
 
 
 function updateDisplay(){
     display.textContent = displayValue;
-}
+};
 
+function calcResult(a, b, c){
+    const num1 = parseFloat(a);
+    const num2 = parseFloat(c);
+    if (b === "+"){
+        currentNumber = num1 + num2;
+    }
+    else if (b === "-"){
+        currentNumber = num1 - num2;
+    }
+    else if (b === "/"){
+        currentNumber = num1 / num2;
+    }
+    else if (b === "*"){
+        currentNumber = num1 * num2;
+    }
+    displayValue = currentNumber;
+    updateDisplay();
+};
+
+
+
+
+
+function clearAll(){
+    currentNumber = "";
+    operator = "";
+    oldNumber = "";
+    displayValue = 0;
+    updateDisplay();
+};
